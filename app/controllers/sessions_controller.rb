966b27d5c03ce05a2b2b7f670fb67fe2b0b0874e
hconlin <hconlinmouat@gmail.com>
2019-11-24 22:52:26 -0800

    renamed migration file

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,10 +8,11 @@ class SessionsController < ApplicationController
        (user.locked == false || user.locked_at <= DateTime.now - (1/24.0))
       session[:user_id] = user.id
       user.login_attempts = 0
+      user.locked = false
       redirect_to admin_panel_path
     else
       user.increment!(:login_attempts)
-      if user.login_attempts = 4
+      if user.login_attempts == 4
         user.locked = true
         user.locked_at = DateTime.now
         flash[:danger] = "Your account has been locked."
