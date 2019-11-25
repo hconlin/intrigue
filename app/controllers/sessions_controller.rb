97d47cefc4b7d74a5fb25f4661e3f3c6a9c08dcf
hconlin <hconlinmouat@gmail.com>
2019-11-25 09:49:59 -0800

    fixed login attempt counter

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,11 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user.locked == true && user.locked_at > DateTime.now - (1.0/24.0)
-      flash[:danger] = "Your account is locked"
+      time = user.locked_at - DateTime.now - (1.0/24.0)
+      time_left = time.strftime("%I:%M %P")
+      flash[:danger] = "Your account is locked. It will unlock in " + time_left
       redirect_to admin_path
-    # user's account is either not locked or an hour has passed 
+    # user's account is either not locked or an hour has passed
     else
       if user && user.authenticate(params[:password])
         session[:user_id] = user.id
@@ -15,6 +17,10 @@ class SessionsController < ApplicationController
         user.locked = false
         redirect_to admin_panel_path
       else
+        if user.locked_at <= DateTime.now - (1.0/24.0)
+          user.login_attempts = 0
+        end
+        #increment login attempts
         user.increment!(:login_attempts)
         if user.login_attempts == 4
           user.locked = true
